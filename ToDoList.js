79cf590eabcac6c6a4aaf11897fc7f4c6efd6e92
elijahchen <code.elijahchen@gmail.com>
2017-02-18 22:02:13 -0500

    - Allowed the user to delete from the todo list

--- ToDoList.js
+++ ToDoList.js
@@ -4,13 +4,39 @@ var todos = ["Buy new turtle"];
 var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
+	//Handle input
 	if(input === "list"){
-	console.log(todos);
+		listTodos();
 	} else if(input === "new"){
-	var newTodo = prompt("Enter new todo");
-	todos.push(newTodo);
+		addTodo();
+	} else if(input === "delete"){
+		deleteTodo();
 	}
 	input = prompt("What would you like to do?");
 }
 
 console.log("Quiting Application");
+
+function listTodos(){
+	console.log("*****");
+	todos.forEach(function(todo, i) {
+		console.log(i + ": " + todo);
+	});
+	console.log("*****");
+}
+
+function addTodo(){
+	//Prompt for new todo
+	var newTodo = prompt("Enter new todo");
+	//Add to todos array
+	todos.push(newTodo);
+	console.log("Added Todo")
+}
+
+function deleteTodo(){
+	//Ask for index of todo to be deleted
+	var index = prompt("Enter index of todo to delete")
+	//Delete todo using splice
+	todos.splice(index, 1);
+	console.log("Deleted Todo")
+}
